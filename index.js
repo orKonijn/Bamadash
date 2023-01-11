06372a4ceea88d7c3e6a66f2062ef0d9c357e96a
or <okonijn@gmail.com>
2023-01-11 15:27:35 +0200

    added everything but filter

--- index.js
+++ index.js
@@ -1,1 +1,60 @@
-console.log("h");
+const Bamadash = {
+    calc(operand1, operand2, operator) {
+        switch(operator){
+            case '+':
+                return operand1+operand2;
+            case '-':
+                return operand1-operand2;
+            case '*':
+                return operand1*operand2;
+            case '/':
+                return operand1/operand2;
+        }
+    },
+    compact(array){
+        return array.filter(Boolean);
+    },
+    intersection(array1,array2, isStrict){
+        intersection=[]
+        if(isStrict){
+            array1.forEach(element1 => {
+                array2.forEach(element2 => {
+                    if(element1===element2){
+                        intersection.push(element1);
+                    }
+                });
+            });
+            return intersection;
+        }
+        else{
+            array1.forEach(element1 => {
+                array2.forEach(element2 => {
+                    if(element1==element2){
+                        intersection.push(element1);
+                    }
+                });
+            });
+            return intersection;
+
+        }
+    },
+    filter(array) {
+        throw new Error("Method not implimented.");
+    },
+    size(collection) { 
+        if(collection.length==undefined){
+            return 0;
+        }
+        return collection;
+    },
+    castArray(value) {
+        if(Array.isArray(value)){
+            return value;
+        }
+        else{
+            return arr=[value];
+        }
+    }
+}
+console.log(Bamadash.castArray([2]));
+module.exports = Bamadash;
